11b4ed9f232647805846f3b0e2ae0043a97e9b25
afra ht <afra.haouat@esprit.tn>
2026-04-27 22:09:15 +0100

    Ajout de  modification des modèles dans TalentBridge

--- Models/Contrat.php
+++ Models/Contrat.php
@@ -9,6 +9,9 @@ class Contrat
     private $delai;
     private $statut;
     private $date_creation;
+    private $freelance_info;
+    private $signature_client;
+    private $signature_freelance;
 
     public function __construct(
         $id_contrat = null,
@@ -17,7 +20,10 @@ class Contrat
         $budget = null,
         $delai = null,
         $statut = 'brouillon',
-        $date_creation = null
+        $date_creation = null,
+        $freelance_info = null,
+        $signature_client = null,
+        $signature_freelance = null
     ) {
         $this->id_contrat = $id_contrat;
         $this->titre = $titre;
@@ -26,6 +32,9 @@ class Contrat
         $this->delai = $delai;
         $this->statut = $statut;
         $this->date_creation = $date_creation;
+        $this->freelance_info = $freelance_info;
+        $this->signature_client = $signature_client;
+        $this->signature_freelance = $signature_freelance;
     }
 
     // Getters
@@ -36,6 +45,9 @@ class Contrat
     public function getDelai() { return $this->delai; }
     public function getStatut() { return $this->statut; }
     public function getDateCreation() { return $this->date_creation; }
+    public function getFreelanceInfo() { return $this->freelance_info; }
+    public function getSignatureClient() { return $this->signature_client; }
+    public function getSignatureFreelance() { return $this->signature_freelance; }
 
     // Setters
     public function setIdContrat($id_contrat) { $this->id_contrat = $id_contrat; }
@@ -45,4 +57,7 @@ class Contrat
     public function setDelai($delai) { $this->delai = $delai; }
     public function setStatut($statut) { $this->statut = $statut; }
     public function setDateCreation($date_creation) { $this->date_creation = $date_creation; }
+    public function setFreelanceInfo($freelance_info) { $this->freelance_info = $freelance_info; }
+    public function setSignatureClient($signature_client) { $this->signature_client = $signature_client; }
+    public function setSignatureFreelance($signature_freelance) { $this->signature_freelance = $signature_freelance; }
 }
